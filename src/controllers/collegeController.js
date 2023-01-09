5391de570dff9b38c3f56911b4a989c15f0567aa
unknown <aniketraj8936@gmail.com>
2023-01-09 16:04:39 +0530

    college

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,26 +1,4 @@
-
-const collegModel=require("../models/collegeModel")
-const validator=require("validator")
-const {isValid}=require("../validation/validation")
-
-const createCollege=async function(req,res){
-    try{
-        let data=req.body
-    const {name,fullName,logoLink}=data
-    if(!isValid(name)) return res.status(400).send({status:false,msg:"please enter the name"})
-    if(!isValid(fullName)) return res.status(400).send({status:false,msg:"please enter the fullName"})
-    if(!logoLink) return res.status(400).send({status:false,msg:"please enter the logoLink"})
-   
-    //if(!isValid(name)) return res.status(400).send({status:false,msg:"invalid Name"})
-    
-    let saveData=await collegModel.create(data)
-     res.status(201).send({status:true,data:saveData})
-    }
-    catch(err){
-        res.status(500).send({status:false,msg:err.messege})
-    }
-    
-const collegeModel = require('../model/collegeModel')
+ const collegeModel = require('../models/collegeModel')
 const validator = require('validator')
 const isValid = function(value) {
     if (typeof value=== "undefined" || typeof value ===null) return false
@@ -44,7 +22,7 @@ const createCollege = async function(req, res) {
         if (!isValid(name)) {
             return res.status(400).send({ status: false, message: "Invalid College name or college name is not mentioned." })
         }
-        if (!/^[a-zA-Z]{3,10}$/.test(name)) {
+        if (!/^[a-zA-Z]{2,10}$/.test(name)) {
             return res.status(400).send({ status: false, message: "Invalid College Name" })
         }
         const duplicateName = await collegeModel.findOne({ name: name.toLowerCase() }) 
@@ -83,4 +61,4 @@ const createCollege = async function(req, res) {
         return res.status(500).send({ status: false, message: error.message })
     }
 }
-module.exports.createCollege = createCollege
+  module.exports.createCollege=createCollege
