16dd130ba16aa14443c019c02965556b209658dd
Abhinav Gupta <98967744+abhinav101901@users.noreply.github.com>
2023-01-09 15:22:35 +0530

    Update collegeController.js

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,3 +1,5 @@
+const collegeModel = require('../model/collegeModel')
+const validator = require('validator')
 const isValid = function(value) {
     if (typeof value=== "undefined" || typeof value ===null) return false
     if (typeof value=== "string" &&  value.trim().length===0) return false
